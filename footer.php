225ba607d893fc4d91c6c45d5d632bb95138e04f
valentingor <v.gorbatenko84@gmail.com>
2021-08-29 12:03:38 +0300

    Add footer blocks.

--- footer.php
+++ footer.php
@@ -1,3 +1,82 @@
+<footer>
+    <div class="footer">
+        <div class="footer__info">
+            <div class="footer__info_block">
+                <div class="footer__item">
+                    <div class="footer__item_head">
+                        <h4>Stay In The Know !</h4>
+                    </div>
+                    <div class="footer__item_info">
+                        <div class="footer__item_info-form">
+                            <span>Please enter your email to join our mailing list</span>
+                            <form>
+                                <div class="form-group">
+                                    <label class="display-none" for="exampleInputEmail1">Email address</label>
+                                    <input type="email"  id="exampleInputEmail1">
+                                </div>
+                                <button type="submit">GO</button>
+                            </form>
+                            <span>To unsubsribe please click here »</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer__item">
+                    <div class="footer__item_head">
+                        <h4>Lacus interdum</h4>
+                    </div>
+                    <div class="footer__item_info">
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                            <li><a href="#">Praesent et eros</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer__item">
+                    <div class="footer__item_head">
+                        <h4>Lacus interdum</h4>
+                    </div>
+                    <div class="footer__item_info">
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                            <li><a href="#">Praesent et eros</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer__item">
+                    <div class="footer__item_head">
+                        <h4>Lacus interdum</h4>
+                    </div>
+                    <div class="footer__item_info">
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                            <li><a href="#">Praesent et eros</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Suspendisse in neque</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__copy">
+            <div class="footer__copy_text">
+                <div class="footer__copy_text-copyright">
+                    <a href="#">Copyright © 2013 Domain Name - All Rights Reserved</a>
+                </div>
+                <div class="footer__copy_text-author">
+                    <a href="#">Template by OS Templates</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
