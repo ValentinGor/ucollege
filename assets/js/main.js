826161338b822011055804cbb8364d16d3f75fa5
valentingor <v.gorbatenko84@gmail.com>
2021-08-30 18:06:36 +0300

    Responsive Design 991

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,1 @@
-$(document).ready(function () {
-    $('.slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 7500,
-    });
-
-    $('[data-fancybox="images"]').fancybox({
-        // Options will go here
-    });
-});
-
+$(document).ready((function(){$(".slider").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:7500}),$('[data-fancybox="images"]').fancybox({})}));
---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 7500,
+    });
+
+    $('[data-fancybox="images"]').fancybox({
+        // Options will go here
+    });
+});
